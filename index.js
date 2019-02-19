ec9bf393060a701d9907cfb79b9e8385bdd73f23
M Haidar Hanif <git@mhaidarhanif.com>
2019-02-19 15:44:31 +0700

    Implement /users

--- index.js
+++ index.js
@@ -2,10 +2,48 @@ const express = require('express')
 const app = express()
 const port = 8000
 
+const users = [
+  {
+    id: 1,
+    name: 'Haidar'
+  },
+  {
+    id: 2,
+    name: 'Bara'
+  },
+  {
+    id: 3,
+    name: 'Ajin'
+  },
+  {
+    id: 4,
+    name: 'Sakti'
+  },
+  {
+    id: 5,
+    name: 'Jonathan'
+  },
+  {
+    id: 6,
+    name: 'Mario'
+  },
+  {
+    id: 7,
+    name: 'Fahri'
+  }
+]
+
 // GET http://localhost:8000/
 app.get('/', (req, res) => {
   res.send({
     message: 'Hello World'
+  })
+})
+
+app.get('/users', (req, res) => {
+  res.send({
+    message: 'List of users',
+    data: users
   })
 })
 
