9e5eb40bde3cbf3838c496727bd58c3969756fd4
M Haidar Hanif <git@mhaidarhanif.com>
2019-02-19 15:16:11 +0700

    Create express app

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Learn Express");
+const express = require('express')
+const app = express()
+const port = 8000
+
+// GET http://localhost:8000/
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+app.listen(port, () => {
+  console.log(`Express app is listening on localhost:${port}`)
+})
