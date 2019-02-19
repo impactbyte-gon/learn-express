6d12d2b27644d2b855e4f087e70027c1e952e56d
M Haidar Hanif <git@mhaidarhanif.com>
2019-02-19 16:12:54 +0700

    Implement create new user

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 8000
 
-const users = [
+// -----------------------------------------------------------------------------
+// EXPRESS PLUGINS
+
+// Use body-parser into express
+app.use(bodyParser.json())
+
+// -----------------------------------------------------------------------------
+// DATA
+
+let nextId = 8
+let users = [
   {
     id: 1,
     name: 'Haidar'
@@ -33,19 +44,51 @@ const users = [
   }
 ]
 
-// GET http://localhost:8000/
+// -----------------------------------------------------------------------------
+// EXPRESS ROUTES/ENDPOINTS
+
+// Get hello world
 app.get('/', (req, res) => {
   res.send({
     message: 'Hello World'
   })
 })
 
+// List all users
 app.get('/users', (req, res) => {
   res.send({
     message: 'List of users',
     data: users
   })
 })
+
+// Create new user
+app.post('/users', (req, res) => {
+  // Check if name in request body is exist
+  if (req.body.name) {
+    // Get new user data from request
+    const newUser = {
+      id: nextId,
+      name: req.body.name
+    }
+
+    // Concat new user into newUsers variable
+    // Then replace old users with new users
+    users = users.concat(newUser)
+    // Increment nextId
+    nextId++
+
+    // Send response
+    res.send({
+      message: 'Created new user',
+      newUser: newUser,
+      data: users
+    })
+  }
+})
+
+// -----------------------------------------------------------------------------
+// RUN EXPRESS
 
 app.listen(port, () => {
   console.log(`Express app is listening on localhost:${port}`)
