54a612e721445e024205bb6176593834fe0cd0d1
M Haidar Hanif <git@mhaidarhanif.com>
2019-02-21 11:29:34 +0700

    Put more data

---
+++ middlewares/todos/data.js
@@ -0,0 +1,21 @@
+module.exports = {
+  nextId: 8,
+  users: [
+    {
+      id: 1,
+      text: 'learning javascript'
+    },
+    {
+      id: 2,
+      text: 'learning javascript'
+    },
+    {
+      id: 3,
+      text: 'learning javascript'
+    },
+    {
+      id: 4,
+      text: 'learning javascript'
+    }
+  ]
+}
--- middlewares/users/data.js
+++ middlewares/users/data.js
@@ -3,11 +3,20 @@ module.exports = {
   users: [
     {
       id: 1,
-      name: 'Haidar'
+      name: 'Haidar',
+      age: 25,
+      phone: ['021 7753904', '021 912892912901'],
+      address: {
+        detail: 'Jl. klkalkslalsa',
+        city: 'Depok',
+        country: 'Indonesia'
+      },
+      todos: [1, 2, 4]
     },
     {
       id: 2,
-      name: 'Bara'
+      name: 'Bara',
+      todos: [3]
     },
     {
       id: 3,
